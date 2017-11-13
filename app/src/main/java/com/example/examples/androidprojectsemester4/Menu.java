7cb2c3bedbaa22eecaec5e57013667af7c6c9aab
odinchev <oleg_dinchev@abv.bg>
2017-11-13 12:03:26 +0200

    added the client

---
+++ app/src/main/java/com/example/examples/androidprojectsemester4/Menu.java
@@ -0,0 +1,53 @@
+package com.example.examples.androidprojectsemester4;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        /*
+        setContentView(R.layout.activity_menu);
+        Toolbar mytoolbar=(Toolbar)findViewById(R.id.ToolBar);
+        setSupportActionBar(mytoolbar);
+        ActionBar ab=getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        **/
+    }
+    /*
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.view2:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast toast= Toast.makeText(this,"Android Toast",Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
+    }
+    **/
+}
