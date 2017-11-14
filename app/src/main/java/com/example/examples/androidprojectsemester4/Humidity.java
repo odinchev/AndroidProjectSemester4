ccfc96cb809c3314d2cdab79114cdca60be661a6
odinchev <oleg_dinchev@abv.bg>
2017-11-14 18:41:26 +0200

    added the client

--- app/src/main/java/com/example/examples/androidprojectsemester4/Humidity.java
+++ app/src/main/java/com/example/examples/androidprojectsemester4/Humidity.java
@@ -1,5 +1,6 @@
 package com.example.examples.androidprojectsemester4;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Handler;
 import android.os.Looper;
@@ -8,6 +9,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
@@ -71,7 +73,37 @@ public class Humidity extends AppCompatActivity implements MqttCallback
         getMenuInflater().inflate(R.menu.menu,menu);
         return super.onCreateOptionsMenu(menu);
     }
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        switch (item.getItemId())
+        {
+            case R.id.view1:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast humidity= Toast.makeText(this,"Android Toast",Toast.LENGTH_LONG);
+                humidity.show();
+                Intent humidityIntent=new Intent (this,Humidity.class);
+                startActivity(humidityIntent);
+                return true;
+
+            case R.id.view2:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast temperature= Toast.makeText(this,"Android Toast",Toast.LENGTH_LONG);
+                temperature.show();
+                Intent temperatureIntent=new Intent (this,MainActivity.class);
+                startActivity(temperatureIntent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
+    }
 
 
     @Override
